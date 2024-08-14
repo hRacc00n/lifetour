0b4222356a61e62f5df83d583a19aedc0a33c995
Aleksey Krasavin <142975839+hRacc00n@users.noreply.github.com>
2024-08-14 13:38:43 +0300

    Создает модуль для слайдеров

--- source/js/main.js
+++ source/js/main.js
@@ -5,28 +5,7 @@
 
 import { onBurgerSwitchDisplay } from './on-burger-switch-display';
 import { burgerButton } from './variables';
-// Swiper
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import { swiperHero } from './swipers';
 
 burgerButton.addEventListener('click', onBurgerSwitchDisplay);
-
-const swiper = new Swiper('.hero__swiper', {
-  modules: [Pagination],
-  pagination: {
-    el: '.hero__paginations',
-    clickable: true,
-  },
-  breakpoints: {
-    320: {
-      loop: true,
-      simulateTouch: true,
-    },
-    1440: {
-      simulateTouch: false,
-    }
-  }
-});
+swiperHero.update();
