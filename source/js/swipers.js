e82a6ce4886fd42def1168aa054c5880f24bd3cc
hRacc00n <MrAlexKr@gmail.com>
2024-08-26 11:30:44 +0300

    Верстает блок adv

--- source/js/swipers.js
+++ source/js/swipers.js
@@ -108,7 +108,7 @@ const swiperAdv = new Swiper('.adv__swiper', {
     1440: {
       enabled: true,
       slidesPerView: 'auto',
-      initialSlide: 4,
+      initialSlide: 3,
       spaceBetween: 30,
       centeredSlides: true,
       loop: true,
@@ -117,27 +117,5 @@ const swiperAdv = new Swiper('.adv__swiper', {
     }
   }
 });
-
-// window.addEventListener('resize', () => {
-//   const advItems = document.querySelectorAll('.adv__item');
-//   const advList = document.querySelector('.adv__list');
-
-//   if (window.screen.width >= 1440) {
-//     advList.classList.add('swiper-wrapper');
-//     advItems.forEach((element) => {
-//       if (!element.classList.contains('swiper-slide')) {
-//         element.classList.add('swiper-slide');
-//       }
-//     });
-//   } else {
-//     swiperAdv.disable();
-//     advList.classList.remove('swiper-wrapper');
-//     advItems.forEach((element) => {
-//       if (element.classList.contains('swiper-slide')) {
-//         element.classList.remove('swiper-slide');
-//       }
-//     });
-//   }
-// });
 
 export { swiperHero, swiperTours, swiperTraining, swiperReviews, swiperAdv };
